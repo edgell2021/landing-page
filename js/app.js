5e63f5da2e74b68ea4d5879ee9035a2d54756b7a
edgell2021 <joedge2020@gmail.com>
2020-01-11 13:34:46 -0500

    refactor js so navigation stays on scroll up

--- js/app.js
+++ js/app.js
@@ -41,14 +41,15 @@ returnBtn.addEventListener("click", function() {
 //detect Scroll stop and hide navigation based set Timeout code on W3 schools (https://www.w3schools.com/jsref/met_win_cleartimeout.asp) and stack overflow(https://stackoverflow.com/questions/3701311/event-when-user-stops-scrolling/3701328)
 let time;
 
-if (window.scrollY <= 400) {
-  window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function() {
+  navCont.style.display = "block";
+  if (window.scrollY > 400) {
     clearTimeout(time);
     time = setTimeout(stopScroll, 1000);
     navCont.style.display = "block";
-  });
+  }
+});
 
-  let stopScroll = function() {
-    navCont.style.display = "none";
-  };
-}
+let stopScroll = function() {
+  navCont.style.display = "none";
+};
