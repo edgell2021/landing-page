b0202094760b71c1bfdcb4c39516a063f99b7c93
edgell2021 <joedge2020@gmail.com>
2020-01-12 08:49:55 -0500

    remove scroll code

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 View example page at: https://edgell2021.github.io/landing-page/
 
-Initial HTML and CSS provided by Udacity. Used JS to dynamically build navigation as unordered list, scroll to approproiate section on click, chnage active class of section, return to top of page on click of return button, and hide navigation on scroll.
+Initial HTML and CSS provided by Udacity. Used JS to dynamically build navigation as unordered list, scroll to approproiate section on click, chnage active class of section, and return to top of page on click of return button.
 
 Used Cupcake Ipsum - http://www.cupcakeipsum.com/
--- js/app.js
+++ js/app.js
@@ -37,19 +37,3 @@ const returnBtn = document.querySelector(".return-button");
 returnBtn.addEventListener("click", function() {
   window.scroll(0, pageTop.offsetTop - navCont.offsetHeight);
 });
-
-//detect Scroll stop and hide navigation based set Timeout code on W3 schools (https://www.w3schools.com/jsref/met_win_cleartimeout.asp) and stack overflow(https://stackoverflow.com/questions/3701311/event-when-user-stops-scrolling/3701328)
-let time;
-
-window.addEventListener("scroll", function() {
-  navCont.style.display = "block";
-  if (window.scrollY > 400) {
-    clearTimeout(time);
-    time = setTimeout(stopScroll, 1000);
-    navCont.style.display = "block";
-  }
-});
-
-let stopScroll = function() {
-  navCont.style.display = "none";
-};
